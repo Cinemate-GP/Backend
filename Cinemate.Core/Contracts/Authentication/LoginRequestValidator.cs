cba37dda4d2899054d9e844d0f2cf33b3bcdbfd1
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-02-26 23:05:05 +0200

    add login request and response for login

---
+++ Cinemate.Core/Contracts/Authentication/LoginRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Cinemate.Core.Contracts.Authentication
+{
+	public class LoginRequestValidator : AbstractValidator<LoginRequest>
+	{
+		public LoginRequestValidator()
+		{
+			RuleFor(x => x.Email)
+				.NotEmpty()
+				.EmailAddress();
+
+			RuleFor(x => x.Password)
+				.NotEmpty();
+		}
+	}
+}
