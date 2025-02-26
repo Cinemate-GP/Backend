d754e455d0df32ca6c034de7434c3c8394e319bf
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-02-26 23:05:39 +0200

    add configuration fro database

---
+++ Cinemate.Repository/Migrations/20250226203646_AddDefaultRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Cinemate.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "IsDefault", "IsDeleted", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "45781365-655c-42b4-aae7-897e4ea7f834", "86b1eaf4-1099-4170-86d5-b30162221da9", true, false, "Member", "MEMBER" },
+                    { "e1940bc8-a54c-494d-9286-a585466c73f0", "f51e5a91-bced-49c2-8b86-c2e170c0846c", false, false, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "45781365-655c-42b4-aae7-897e4ea7f834");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e1940bc8-a54c-494d-9286-a585466c73f0");
+        }
+    }
+}
