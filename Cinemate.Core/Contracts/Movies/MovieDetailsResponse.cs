adcaca166b3ca3f9ff68b2f84c08ac8986e3db8d
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-03-16 07:24:01 +0200

    add response for movie

---
+++ Cinemate.Core/Contracts/Movies/MovieDetailsResponse.cs
@@ -0,0 +1,22 @@
+﻿using Cinemate.Core.Contracts.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemate.Core.Contracts.Movies
+{
+	public record MovieDetailsResponse
+	(
+		int MovieId,
+		int TMDBId,
+		string? title,
+		string? Overview,
+		string? Poster_path,
+		int? Runtime,
+		DateOnly? Release_date,
+		string? Trailer_path,
+		IEnumerable<ActorMovieResponse> Actors
+	);
+}
---
+++ Cinemate.Core/Errors/Movie/MovieErrors.cs
@@ -0,0 +1,15 @@
+﻿using Cinemate.Repository.Abstractions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemate.Core.Errors.Movie
+{
+	public static class MovieErrors
+    {
+		public static readonly Error MovieNotFound = new("Movie.NotFound", "No Movie was found with the given id", StatusCodes.Status404NotFound);
+	}
+}
