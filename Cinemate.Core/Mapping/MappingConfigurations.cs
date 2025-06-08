cd2ffbbde840b26aa932eda477e0b8bf587ed5f9
Ziad <ziad@example.com>
2025-06-08 05:23:54 +0200

    using mapster in movie and actor respones

--- Cinemate.Core/Mapping/MappingConfigurations.cs
+++ Cinemate.Core/Mapping/MappingConfigurations.cs
@@ -3,11 +3,6 @@ using Cinemate.Core.Contracts.Genres;
 using Cinemate.Core.Contracts.Movies;
 using Cinemate.Core.Entities;
 using Mapster;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cinemate.Core.Mapping
 {
@@ -25,11 +20,12 @@ namespace Cinemate.Core.Mapping
 						cm.Cast.ProfilePath,
 						cm.Role,
 						cm.Extra)))
-				.Map(dest => dest.MovieReviews, src => new List<MovieReviewResponse>())
-				.Map(dest => dest.IsLiked, src => false)
-				.Map(dest => dest.IsInWatchList, src => false)
-				.Map(dest => dest.IsWatched, src => false)
-				.Map(dest => dest.Stars, src => (int?)null);
+				.Map(dest => dest.TMDBId, src => src.TMDBId)
+				.Map(dest => dest.IMDBRating, src => string.IsNullOrEmpty(src.IMDBRating) ? "0.0/10" : src.IMDBRating)
+				.Map(dest => dest.RottenTomatoesRating, src => string.IsNullOrEmpty(src.RottenTomatoesRating) ? null : src.RottenTomatoesRating)
+				.Map(dest => dest.MetacriticRating, src => string.IsNullOrEmpty(src.MetacriticRating) ? null : src.MetacriticRating)
+				.Map(dest => dest.MPA, src => string.IsNullOrEmpty(src.MPA) ? "Not Rated" : src.MPA)
+				.Map(dest => dest.MovieReviews, src => new List<MovieReviewResponse>());
 
 			config.NewConfig<Movie, MoviesTopTenResponse>()
 				.Map(dest => dest.TMDBId, src => src.TMDBId)
@@ -39,13 +35,27 @@ namespace Cinemate.Core.Mapping
 			config.NewConfig<Movie, MovieTrendingResponse>()
 				.Map(dest => dest.GenresDetails, src =>
 					src.MovieGenres.Select(mg => new GenresDetails(mg.Genre.Id, mg.Genre.Name ?? string.Empty)))
-				.Map(dest => dest.IMDBRating, src => src.IMDBRating == null ? "0.0/10" : src.IMDBRating);
+				.Map(dest => dest.IMDBRating, src => src.IMDBRating == null ? "0.0/10" : src.IMDBRating)
+				.Map(dest => dest.TMDBId, src => src.TMDBId);
 
 			config.NewConfig<Movie, MovieDetailsRandomResponse>()
 				.Map(dest => dest.GenresDetails, src =>
 					src.MovieGenres.Select(mg => new GenresDetails(mg.Genre.Id, mg.Genre.Name ?? string.Empty)))
 				.Map(dest => dest.IMDBRating, src => src.IMDBRating == null ? "0.0/10" : src.IMDBRating)
-				.Map(dest => dest.ReleasDate, src => src.ReleaseDate);
+				.Map(dest => dest.ReleasDate, src => src.ReleaseDate)
+				.Map(dest => dest.TMDBId, src => src.TMDBId);
+
+			config.NewConfig<Cast, ActorDetailsResponse>()
+				.Map(dest => dest.Id, src => src.CastId)
+				.Map(dest => dest.Name, src => src.Name)
+				.Map(dest => dest.Biography, src => src.Biography)
+				.Map(dest => dest.BirthDay, src => src.BirthDay)
+				.Map(dest => dest.DeathDay, src => src.DeathDay)
+				.Map(dest => dest.ProfilePath, src => src.ProfilePath)
+				.Map(dest => dest.PlaceOfBirth, src => src.PlaceOfBirth)
+				.Map(dest => dest.Popularity, src => src.Popularity)
+				.Map(dest => dest.KnownForDepartment, src => src.KnownForDepartment)
+				.Map(dest => dest.Movies, src => new List<MoviesTopTenResponse>());
 
 			config.NewConfig<Cast, ActorDetailsResponse>();
 
--- Cinemate.Service/Services/Actors/ActorService.cs
+++ Cinemate.Service/Services/Actors/ActorService.cs
@@ -6,25 +6,16 @@ using Cinemate.Core.Repository_Contract;
 using Cinemate.Core.Service_Contract;
 using Cinemate.Repository.Abstractions;
 using Cinemate.Repository.Data.Contexts;
-using MapsterMapper;
+using Mapster;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cinemate.Service.Services.Actors
 {
-    public class ActorService : IActorService
+	public class ActorService : IActorService
 	{
-		private readonly IUnitOfWork _unitOfWork;
-		private readonly IMapper _mapper;
 		private readonly ApplicationDbContext _context;
-		public ActorService(IUnitOfWork unitOfWork, IMapper mapper, ApplicationDbContext context)
+		public ActorService(ApplicationDbContext context)
 		{
-			_unitOfWork = unitOfWork;
-			_mapper = mapper;
 			_context = context;
 		}
 		public async Task<Result<ActorDetailsResponse>> GetActorDetailsAsync(int actorid, CancellationToken cancellationToken = default)
@@ -37,30 +28,14 @@ namespace Cinemate.Service.Services.Actors
 			if (actor is null)
 				return Result.Failure<ActorDetailsResponse>(ActorErrors.ActorNotFound);
 
+			var actorDetails = actor.Adapt<ActorDetailsResponse>();
 			var movies = actor.CastMovies
 				.OrderByDescending(cm => cm.Movie.ReleaseDate)
 				.Where(m => m.Movie.IsDeleted != true && m.Movie.PosterPath != null && m.Movie.Trailer != null)
-				.Select(cm => new MoviesTopTenResponse(
-					cm.Movie.TMDBId,
-					cm.Movie.Title ?? string.Empty,
-					cm.Movie.PosterPath,
-                    cm.Movie.IMDBRating,
-                    cm.Movie.MPA
-                ))
+				.Select(cm => cm.Movie.Adapt<MoviesTopTenResponse>())
 				.ToList();
 
-			var result = new ActorDetailsResponse(
-				actor.CastId,
-                actor.Name,
-                actor.Biography,
-                actor.BirthDay,
-                actor.DeathDay,
-                actor.ProfilePath,
-                actor.PlaceOfBirth,
-                actor.Popularity,
-                actor.KnownForDepartment,
-                movies
-            );
+			var result = actorDetails with { Movies = movies };
 
 			return Result.Success(result);
 		}
