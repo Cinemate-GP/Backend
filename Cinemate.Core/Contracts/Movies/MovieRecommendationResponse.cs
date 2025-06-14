f4d2090f13afed15cd73348d8b18f0e6ac9a4f2f
Ziad <ziad@example.com>
2025-06-14 18:57:56 +0300

    add movie recommender endpoint

---
+++ Cinemate.Core/Contracts/Movies/MovieRecommendationResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemate.Core.Contracts.Movies
+{
+	public record MovieRecommendationResponse
+	(
+		int TMDBId,
+		double Score
+	);
+}
